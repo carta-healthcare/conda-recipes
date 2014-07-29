3f6d09c2dd7a0b5ec052e2f79ec9fa993e24f611
Matt Hollingsworth <matt@deepfield.net>
2014-07-29 16:20:30 -0400

    Also build pylibpcapwq

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for pkg in "httplib2 anyjson billiard ipdb funcy dpkt-fix markdown2 amqp kombu pcapy argcomplete apache-libcloud boto dnspython py-bcrypt pycrypto graphviz pygraphviz python-dateutil python-levenshtein pytz scapy simplejson slimit zope.cachedescriptors paramiko pexpect fabric bottleneck celery cov-core formencode geopy ipaddr influxdb m2crypto msgpack-python netaddr networkx nose-cov oauth2 thrift slimit pyquery"; do
+for pkg in "httplib2 anyjson billiard ipdb funcy dpkt-fix markdown2 amqp kombu pcapy argcomplete apache-libcloud boto dnspython py-bcrypt pycrypto graphviz pygraphviz python-dateutil python-levenshtein pytz scapy simplejson slimit zope.cachedescriptors paramiko pexpect fabric bottleneck celery cov-core formencode geopy ipaddr influxdb m2crypto msgpack-python netaddr networkx nose-cov oauth2 thrift slimit pyquery pylibpcap"; do
 
    conda build $pkg;
 
