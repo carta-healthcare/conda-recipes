a14544823badcb61491f2a6fdea3b9d404007cab
Matt Hollingsworth <matt@deepfield.net>
2014-08-18 21:35:39 -0400

    Add mysql-python, pika, tornado-celery

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for pkg in "httplib2 anyjson billiard ipdb funcy dpkt-fix markdown2 amqp kombu pcapy argcomplete apache-libcloud boto dnspython py-bcrypt pycrypto graphviz pygraphviz python-dateutil python-levenshtein pytz pysnmp simplejson slimit zope.cachedescriptors paramiko pexpect fabric bottleneck celery cov-core formencode geopy ipaddr scapy influxdb m2crypto msgpack-python netaddr networkx nose-cov oauth2 thrift slimit pyquery pylibpcap protobuf hyperloglog parsedatetime arrow salt"; do
+for pkg in "httplib2 anyjson billiard ipdb funcy dpkt-fix markdown2 amqp kombu pcapy argcomplete apache-libcloud boto dnspython py-bcrypt pycrypto graphviz pygraphviz python-dateutil python-levenshtein pytz pysnmp simplejson slimit zope.cachedescriptors paramiko pexpect fabric bottleneck celery cov-core formencode geopy ipaddr scapy influxdb m2crypto msgpack-python netaddr networkx nose-cov oauth2 thrift slimit pyquery pylibpcap protobuf hyperloglog parsedatetime arrow mysql-python pika tornado-celery salt"; do
 
    conda build $pkg;
 
