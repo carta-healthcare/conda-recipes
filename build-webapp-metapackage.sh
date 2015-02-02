400621d38238e93847cbd53b0096613c8bfcc890
Matt Hollingsworth <matt@deepfield.net>
2015-02-01 22:27:54 -0500

    Add script for building webapp metapackage

---
+++ build-webapp-metapackage.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+conda metapackage mhworth-webapp 1.0 --dependencies 'requests =2.2.1' \
+'simplejson =2.6.2' \
+'funcy =1.1' \
+'xlsxwriter =0.5.2' \
+'markdown2 =2.2.1' \
+'boto =2.9.0' \
+'oauth2 =1.5.211' \
+'pexpect =3.0' \
+'fabric =1.9.0' \
+'psutil =1.2.1' \
+'python-levenshtein =0.11.2' \
+'numexpr =2.3.1' \
+'apache-libcloud =0.14.1' \
+'zope.cachedescriptors =4.0.0' \
+'pycrypto =2.6.1' \
+'paramiko =1.12.1' \
+'pygraphviz =1.2' \
+'redis-py' \
+'msgpack-python =0.4.2' \
+'influxdb =0.1.11' \
+'cython' \
+'numpy' \
+'ipython' \
+'ipython-notebook' \
+'parsedatetime =1.4' \
+'python-dateutil =2.2' \
+'PyYAML =3.11' \
+'ipaddr =2.1.11' \
+'scipy' \
+'pandas' \
+'geopy =1.0.0' \
+'pytables' \
+'ipdb =0.8' \
+'arrow' \
+'cython =0.19.2' \
+'scons' \
+'mock' \
+'nose' \
+'nose-cov' \
+'pylint' \
+'MySQL-python =1.2.5' \
+'sqlalchemy =0.9.7' \
+'futures =2.1.6' \
+'django =1.7.3' \
+'djangorestframework =3.0.4' \
+'gunicorn'
